80a4612678dccf3c78ee3a38d373d6301b5227b8
kayemba <kayembejonathan5@gmail.com>
2023-06-21 20:19:28 +0200

    fix linters

--- morse.rb
+++ morse.rb
@@ -1,56 +1,64 @@
-MORSE_CODE_MAP = {
-    ".-" => "A",
-    "-..." => "B",
-    "-.-." => "C",
-    "-.." => "D",
-    "." => "E",
-    "..-." => "F",
-    "--." => "G",
-    "...." => "H",
-    ".." => "I",
-    ".---" => "J",
-    "-.-" => "K",
-    ".-.." => "L",
-    "--" => "M",
-    "-." => "N",
-    "---" => "O",
-    ".--." => "P",
-    "--.-" => "Q",
-    ".-." => "R",
-    "..." => "S",
-    "-" => "T",
-    "..-" => "U",
-    "...-" => "V",
-    ".--" => "W",
-    "-..-" => "X",
-    "-.--" => "Y",
-    "--.." => "Z"
+class MorseCodeDecoder
+  MORSE_CODE_MAP = {
+    '.-' => 'A',
+    '-...' => 'B',
+    '-.-.' => 'C',
+    '-..' => 'D',
+    '.' => 'E',
+    '..-.' => 'F',
+    '--.' => 'G',
+    '....' => 'H',
+    '..' => 'I',
+    '.---' => 'J',
+    '-.-' => 'K',
+    '.-..' => 'L',
+    '--' => 'M',
+    '-.' => 'N',
+    '---' => 'O',
+    '.--.' => 'P',
+    '--.-' => 'Q',
+    '.-.' => 'R',
+    '...' => 'S',
+    '-' => 'T',
+    '..-' => 'U',
+    '...-' => 'V',
+    '.--' => 'W',
+    '-..-' => 'X',
+    '-.--' => 'Y',
+    '--..' => 'Z'
   }.freeze
 
-def decode_char(code)
-  MORSE_CODE_MAP[code]
+  def decode_char(code)
+    MORSE_CODE_MAP[code]
+  end
+
+  def decode_word(morse_word)
+    morse_chars = morse_word.split
+    decoded_word = ''
+    morse_chars.each do |morse_char|
+      decoded_char = decode_char(morse_char)
+      decoded_word += decoded_char if decoded_char
+    end
+    decoded_word
+  end
+
+  def decode(message)
+    morse_words = message.split('   ')
+    decoded_message = ''
+    morse_words.each do |morse_word|
+      decoded_word = decode_word(morse_word)
+      decoded_message += "#{decoded_word} " unless decoded_word.empty?
+    end
+    decoded_message.strip
+  end
 end
 
-char = decode_char('.-')
+decoder = MorseCodeDecoder.new
+char = decoder.decode_char('.-')
 puts char
 
-def decode_word(morse_word)
-  morse_chars = morse_word.split
-  decode_words = ''
-  morse_chars.each do |c|
-    decode_words += decode_char(c)
-  end
-  decode_words
-end
-puts decode_word('-- -.--')
+decoded_word = decoder.decode_word('-- -.--')
+puts decoded_word
 
-def decode(morse_word)
-  splitted = morse_word.split('   ')
-  decode_words = ''
-  splitted.each do |s|
-    decode_words += "#{decode_word(s)} "
-  end
-  decode_words
-end
-puts decode('-- -.--   -. .- -- .')
-puts decode('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
+decoded_message = decoder.decode('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
+puts decoded_message
